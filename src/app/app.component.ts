baaaf40ce336d5a17b596a6753fd4214fdebe459
Annica Thach <annica559@mail.fresnostate.edu>
2019-11-06 16:35:44 -0800

    add main header, footer and content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  // tslint:disable-next-line: component-selector
+  selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'LabAssignment3';
+
+  constructor() {
+
+  }
+
 }
